59104973324d35a28d6ccaa27d5ff8c74112093b
jawsnnn <arpanmalviya@gmail.com>
2021-10-30 23:55:11 -0400

    init working spa

---
+++ components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <nav class = "navbar navbar-expand-lg navbar-dark bg-dark shadow">
+        <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+        <NuxtLink to="/about" class="navbar-brand">About</NuxtLink>
+      </nav>
+      <div class="mt-8 bg-white overflow-hidden sm:rounded-lg p-6">
+        <p class = "mt-3 text-gray-600">
+          {{ message }}
+        </p>
+      </div>
+      </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "About this"
+    }
+  }
+}
+</script>
---
+++ components/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <nav class = "navbar navbar-expand-lg navbar-dark bg-dark shadow">
+        <NuxtLink to="/" class="navbar-brand">Home</NuxtLink>
+        <NuxtLink to="/about" class="nav-link">About</NuxtLink>
+      </nav>
+      <div class="mt-8 bg-white overflow-hidden sm:rounded-lg p-6">
+        <p class = "mt-3 text-gray-600">
+          {{ message }}
+        </p>
+      </div>
+      </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "this is a message"
+    }
+  }
+}
+</script>
+
+<style>
+.navbar-brand {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.nav-link:hover {
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: pink;
+  --tw-text-opacity: 1;
+}
+.nav-link {
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: rgba(0,220,130,var(--tw-text-opacity));
+  --tw-text-opacity: 1;
+}
+</style>
